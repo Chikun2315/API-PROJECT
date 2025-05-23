bed470411c0ac3994cb83f740f6ee70246c69720
Chikun2315 <sankarsanasahoo2315@gmail.com>
2025-05-23 10:29:53 +0530

    login module added

--- src/main/java/com/authService/Config/AuthConfig.java
+++ src/main/java/com/authService/Config/AuthConfig.java
@@ -1,6 +1,12 @@
 package com.authService.Config;
 
+import com.authService.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,8 +17,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthConfig {
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     String[] openUrls = {
             "/api/v1/auth/register",
+            "/api/v1/auth/login",
+            "/api/v1/welcome/msg",
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
@@ -27,8 +38,20 @@ public class AuthConfig {
    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.authorizeHttpRequests(req->{
           req.requestMatchers(openUrls).permitAll().anyRequest().authenticated();
-        }).csrf().disable();
+        }).csrf().disable().httpBasic().disable();
         return http.build();
+   }
+   @Bean
+   public AuthenticationManager authManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+   }
+
+   @Bean
+   public AuthenticationProvider AauthProvider (){
+       DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+       authProvider.setUserDetailsService(customUserDetailsService);
+       authProvider.setPasswordEncoder(getEncode());
+       return authProvider;
    }
 
 }
---
+++ src/main/java/com/authService/Controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.authService.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/welcome")
+public class WelcomeController {
+
+    @GetMapping("/msg")
+    public String getmsg(){
+        return "hello world";
+    }
+}
